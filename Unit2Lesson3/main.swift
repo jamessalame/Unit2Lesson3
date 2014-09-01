ca22451183f9c6cdfc56fc71c29e98f95b1ae62e
James Salame <jimmy@salame.us>
2014-09-01 17:40:29 -0400

    Initial Commit

---
+++ Unit2Lesson3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unit2Lesson3
+//
+//  Created by James Salame on 9/1/14.
+//  Copyright (c) 2014 Salame. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
